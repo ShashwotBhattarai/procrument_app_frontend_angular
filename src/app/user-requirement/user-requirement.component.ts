365589920cee193ad4147bf36c7938ded7f89dfb
Shashwot Bhattarai <shashwot.tech@gmail.com>
2023-07-29 14:15:23 +0545

    form controls all working except

--- src/app/user-requirement/user-requirement.component.ts
+++ src/app/user-requirement/user-requirement.component.ts
@@ -8,14 +8,25 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class UserRequirementComponent implements OnInit {
   nestedForm!: FormGroup;
-  
+
+  // Sample data for autocomplete options
+  siteOptions = [
+    { id: 1, name: 'Site 1' },
+    { id: 2, name: 'Site 2' },
+    { id: 3, name: 'Site 3' },
+  ];
+
+  itemOptions = [
+    { id: 1, name: 'Item A' },
+    { id: 2, name: 'Item B' },
+    { id: 3, name: 'Item C' },
+  ];
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.nestedForm = this.formBuilder.group({
-      outerField: ['', Validators.required],
-      innerFields: this.formBuilder.array([])
+      innerFields: this.formBuilder.array([]) // FormArray for dynamically generated form groups
     });
 
     // Add an initial nested form group
@@ -26,25 +37,35 @@ export class UserRequirementComponent implements OnInit {
     return this.nestedForm.get('innerFields') as FormArray;
   }
 
-  addNestedFormGroup(): void {
-    const nestedFormGroup = this.formBuilder.group({
+  createNestedFormGroup(): FormGroup {
+    return this.formBuilder.group({
       innerField1: ['', Validators.required],
-      innerField2: ['', Validators.required]
+      innerField2: ['', Validators.required],
+      innerField3: ['', Validators.required],
+      innerField4: ['', Validators.required]
     });
+  }
 
+  addNestedFormGroup(): void {
+    const nestedFormGroup = this.createNestedFormGroup();
     this.innerFields.push(nestedFormGroup);
+    this.nestedForm.updateValueAndValidity();
   }
 
   removeNestedFormGroup(index: number): void {
     this.innerFields.removeAt(index);
+    this.nestedForm.updateValueAndValidity();
   }
 
   onSubmit(): void {
     if (this.nestedForm.valid) {
+    
       // Handle form submission
-    }
+      console.log("inside on submit");
+      const formData = this.nestedForm.value;
+      console.log(formData);
+      // You can perform further actions, like sending the data to the server.
+    
   }
 }
-
-
-
+}
