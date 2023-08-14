b4df7946ab7a6402b42878526fd3ec53268d08b6
Shashwot Bhattarai <shashwot.tech@gmail.com>
2023-08-14 16:17:43 +0545

    refactored folder structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
-import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
-import { UserRequirementComponent } from './user-requirement/user-requirement.component';
-import { AdminItemsComponent } from './admin-items/admin-items.component';
-import { AdminSiteComponent } from './admin-site/admin-site.component';
-import { AdminCredentialsComponent } from './admin-credentials/admin-credentials.component';
+
+import { UserRequirementComponent } from './User/user-requirement/user-requirement.component';
+import { AdminDashboardComponent } from './Admin/admin-dashboard/admin-dashboard.component';
+import { UserDashboardComponent } from './User/user-dashboard/user-dashboard.component';
+import { AdminItemsComponent } from './Admin/admin-items/admin-items.component';
+import { AdminSiteComponent } from './Admin/admin-site/admin-site.component';
+import { AdminCredentialsComponent } from './Admin/admin-credentials/admin-credentials.component';
+
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
